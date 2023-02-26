e850932403cc21651b316400f84f3fae7d777cfe
Abdul Kader <abdulkaderm36@gmail.com>
2023-02-26 23:28:46 +0400

    added dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Procedural World</div>;
+  return <div className="App bg-[#212121] h-screen">Procedural World</div>;
 }
 
 export default App;
